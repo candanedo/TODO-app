5551a7472427dd16aba1fb3ab9cf1a740812d4da
Eduardo Candanedo <42420295+candanedo@users.noreply.github.com>
2018-10-10 23:20:30 -0500

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    get 'about/index'
+    get 'howitworks/index'
+    resources :widgets
+    devise_for :users
+    get 'welcome/index'
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    resources :activities
+    root 'welcome#index'
+    #resources :users, only: :index
+
+    resources :activities do
+      resources :tasks
+    end
 end
