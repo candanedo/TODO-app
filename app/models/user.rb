ecdf6b49af041195da8fd2ffae938d5568ff314c
Eduardo Candanedo <eduardo.candanedo.94@gmail.com>
2018-10-13 11:08:19 -0500

    Add image active storage to the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,17 @@ class User < ApplicationRecord
 
   has_many :activities
   has_one_attached :avatar
+
+  validates :name, presence: true
+  validate :has_image_attached
+
+  private
+  def has_image_attached
+  	if !avatar.attached?
+  		errors.add(:avatar, "must have a picture")
+  	elsif !avatar.attached? && !avatar.content_type.in?(%w(avatar/jpeg avatar/png))
+  		errors.add(:avatar, "must be of type JPEG or PNG")
+  	end
+  end
 end
 
