cb43d80624442190d67fbace79b7b4279ce60baa
Eduardo Candanedo <eduardo.candanedo.94@gmail.com>
2018-10-17 19:31:26 -0500

    Define methods for omniauthable

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -257,7 +257,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :github, "APP_ID", "APP_SECRET"
+  config.omniauth :github, "263d2afe9abdf7c025b7", "0a424e7efb61091b4035bc9b590ad4d7c2610d37"
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
