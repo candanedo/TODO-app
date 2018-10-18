3393ebabf1acbf10c4968b5de185571a1381bd64
Eduardo Candanedo <eduardo.candanedo.94@gmail.com>
2018-10-18 10:35:03 -0500

    Spec for the activities controller

--- spec/controllers/activities_controller_spec.rb
+++ spec/controllers/activities_controller_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 RSpec.describe AboutController, type: :controller do
 
   describe "GET #index" do
-    it "returns http success" do
+    it "returns all Activities according to the current_user" do
       get :index
       expect(response).to have_http_status(:success)
     end
