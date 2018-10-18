2e7175b93c2d43e24b15c2f1a5e5b0213ad5d259
Eduardo Candanedo <eduardo.candanedo.94@gmail.com>
2018-10-18 10:35:44 -0500

    Spec for tasks controller

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -1,7 +1,9 @@
 require 'rails_helper'
-
-RSpec.describe TasksController, type: :controller do
+require 'tasks'
+RSpec.describe TasksController, "#index" do
 	it "Returns all tasks for the especified Activity" do
+		tasks = Task.new
 	end
 
 end
+ 
