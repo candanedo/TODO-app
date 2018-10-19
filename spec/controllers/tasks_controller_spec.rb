523a6afd7f8b10b461006699fa88540b2f0f16ba
Eduardo Candanedo <eduardo.candanedo.94@gmail.com>
2018-10-19 17:29:17 -0500

    Rspec tests for tasks controllers

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -1,8 +1,10 @@
 require 'rails_helper'
-RSpec.describe TasksController, "#index" do
-	it "Returns all tasks for the especified Activity" do
-		#tasks = Task.new
-	end
-
+RSpec.describe TasksController, type: :controller do
+  describe "GET #index" do
+    it "Renders the view for login, returns 302 not authorize" do
+      get :index
+      expect(response.status).to eq(302)
+    end
+  end
 end
  
