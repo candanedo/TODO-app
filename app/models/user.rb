1fa239e06916eb90e5ca2698ad1a9a18053c9236
Eduardo Candanedo <eduardo.candanedo.94@gmail.com>
2018-10-19 18:52:45 -0500

    User model refactored

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   
-  devise :database_authenticatable, :async, :registerable,
-         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i[facebook]
+  devise :database_authenticatable, :async, :registerable, :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i[facebook]
 
   has_many :activities
   has_one_attached :avatar
