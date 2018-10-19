ed3667e9140a3e2ed9e6352df1bfbbedd4ffc3a6
Eduardo Candanedo <eduardo.candanedo.94@gmail.com>
2018-10-18 20:06:32 -0500

    Users tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
- context 'validations' do
-  
- end
+RSpec.describe "User", type: :model do
+    it "Is valid only with valid attributes" do
+      file = fixture_file_upload(Rails.root.join('spec', 'pictures', 'prueba.jpg'), 'image/jpg')
+      expect(User.new(id: 1, name: "Eduardo Javier Candanedo", email: "eduardo.candanedo.94@gmail.com", password: "edjacaco", avatar: file)).to be_valid
+      # comment1 = post.comments.create!(:body => "first comment")
+      # comment2 = post.comments.create!(:body => "second comment")
+      # expect(post.reload.comments).to eq([comment2, comment1])
+    end
 end
