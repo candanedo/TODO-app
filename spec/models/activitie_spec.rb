e8cd3afc1e5b47eace96806d20cace16725e9df3
Eduardo Candanedo <eduardo.candanedo.94@gmail.com>
2018-10-18 20:05:44 -0500

    Activity tests

--- spec/models/activitie_spec.rb
+++ spec/models/activitie_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe Activitie, type: :model do
- context 'validations' do
+RSpec.describe "Activitie", type: :model do
+ context 'Is valid only with valid attributes' do
 
+expect(User.new(id: 1, name: "Go to de gym", description: "This week needs to be very productive", password: "edjacaco", avatar: file)).to be_valid
  end
 end
