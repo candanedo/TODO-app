53f16b069eabf756bd465fc75bb3660e3195c7df
Eduardo Candanedo <eduardo.candanedo.94@gmail.com>
2018-10-19 17:22:43 -0500

    Add get tasks/index

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,12 @@ Rails.application.routes.draw do
   get 'buildit/index'
   get 'about/index'
   get 'howitworks/index'
+  get 'tasks/index'
   devise_for :users, controllers: { registrations: 'users/registrations', omniauth_callbacks: "users/omniauth_callbacks" }
   get 'welcome/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :activities
   root 'welcome#index'
   mount Sidekiq::Web => '/sidekiq'
-  #resources :users, only: :index
 
   resources :activities do
 	  resources :tasks
