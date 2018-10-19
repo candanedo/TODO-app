ce81db60418c1e0861f801ee899ead91d5f8d65d
Eduardo Candanedo <eduardo.candanedo.94@gmail.com>
2018-10-18 22:31:17 -0500

    Rspec tests for Task model

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,8 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "Task", type: :model do
-    it "is valid with valid attributes and with activity_id define" do
-      activitie = Activitie.find(1)
-      expect(Task.new(id: 1, name: "Go to de gym", description: "This week increase the rythim", initial_date: Time.now.to_s, final_date: Time.now.to_s, activitie_id: activitie)).to be_valid
+    it "Task is valid with valid attributes and with activity_id define" do
+      expect(Task.new(id: 1, name: "Go to de gym", description: "This week increase the rythim", initial_date: Time.now, final_date: Time.now, activitie: Activitie.new)).to be_valid
     end
+    it "Belongs to a Activity" do
+
+	end
 end
