612727fd9b6781eddffb50c24acc68bf177ec0fc
Eduardo Candanedo <eduardo.candanedo.94@gmail.com>
2018-10-18 20:06:10 -0500

    Tasks tests

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,9 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe Task, type: :model do
-  describe "GET #index" do
-    it "renders the index template" do
-    	
+RSpec.describe "Task", type: :model do
+    it "is valid with valid attributes and with activity_id define" do
+      activitie = Activitie.find(1)
+      expect(Task.new(id: 1, name: "Go to de gym", description: "This week increase the rythim", initial_date: Time.now.to_s, final_date: Time.now.to_s, activitie_id: activitie)).to be_valid
     end
-  end
 end
