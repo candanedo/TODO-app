bd8161ae6568fe4702e4ca35b8d3bdcdd15abb25
Eduardo Candanedo <eduardo.candanedo.94@gmail.com>
2018-10-19 17:28:19 -0500

    Get index should render 302 devise not auth

--- spec/controllers/activities_controller_spec.rb
+++ spec/controllers/activities_controller_spec.rb
@@ -1,24 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe ActivitiesController do
+RSpec.describe ActivitiesController, type: :controller do
   describe "GET index" do
-    it "Renders the view, return 200" do
-      expect(response.status).to eq(200)
+    it "Renders the view log in, return 302 not authorize" do
+      get :index
+      expect(response.status).to eq(302)
     end
-  #   it "Render the index template" do
-  #     get :index
-  #     expect(response).to render_template("index")
-  #   end
-  # end
-  # let(:user) { User.new(id: 1, name: "Eduardo Javier Candanedo", email: "eduardo.candanedo.94@gmail.com", password: "12345678") }
-  # before { sign_in user }
-  # describe"Renders login if unauthorize" do
-  #   before { get :index }
-  #   subject { response }
-  #   it { is_expected.to have_http_status 401 } 
-  #   it "redirects to login" do
-  #     expect(response.headers[:location]).to eq new_user_session_path
-  #   end
   end
+  
 end
 
