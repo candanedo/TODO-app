6d12dac3d1b974de73e57809cf9b4c41260a43d0
Eduardo Candanedo <eduardo.candanedo.94@gmail.com>
2018-10-18 22:31:47 -0500

    Rspec tests for User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "User", type: :model do
-    it "Is valid only with valid attributes" do
+    it "User is valid only with valid attributes" do
       file = fixture_file_upload(Rails.root.join('spec', 'pictures', 'prueba.jpg'), 'image/jpg')
       expect(User.new(id: 1, name: "Eduardo Javier Candanedo", email: "eduardo.candanedo.94@gmail.com", password: "edjacaco", avatar: file)).to be_valid
       # comment1 = post.comments.create!(:body => "first comment")
