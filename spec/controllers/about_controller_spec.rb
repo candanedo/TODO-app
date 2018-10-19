42a5adff55d44684465f470e7c3db98d3cb75ddb
Eduardo Candanedo <eduardo.candanedo.94@gmail.com>
2018-10-19 17:26:04 -0500

    Get index should render 200

--- spec/controllers/about_controller_spec.rb
+++ spec/controllers/about_controller_spec.rb
@@ -1,8 +1,7 @@
 require 'rails_helper'
-
 RSpec.describe AboutController, type: :controller do
   describe "GET #index" do
-	it "Renders the view, returns 200" do
+    it "Renders the view, returns 200" do
       get :index
       expect(response.status).to eq(200)
     end
